8be28931234ba05ff57ab45123b1e4612dc6e5f4
doanquangdungwork-rgb <doanquangdung.work@gmail.com>
2026-09-09 23:52:35 +0700

    fix: remove deprecated middleware convention

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
-
-export default clerkMiddleware();
-
-export const config = {
-  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|gif|svg|ico|webp|avif|woff2?|ttf|map|txt|xml|csv|pdf)).*)', '/(api|trpc)(.*)'],
-};
